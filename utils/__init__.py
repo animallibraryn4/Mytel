97207f1a4778be1f89ad7d6db3b1f7959c93f7ea
animallibraryn4 <nikhil7858978052@gmail.com>
2025-12-25 01:06:23 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,6 @@
+from .main_callbacks import register_main_callbacks
+from .ls_callbacks import register_ls_callbacks
 
+def register_callbacks(app):
+    register_main_callbacks(app)
+    register_ls_callbacks(app)
