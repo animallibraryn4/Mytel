94dab417acf373a23787f216fe49a52b0ee7118b
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-13 19:38:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ API_ID = 22299340
 API_HASH = "09b09f3e2ff1306da4a19888f614d937"
 BOT_TOKEN = ""
 #Logs
-LOG_CHANNEL = "" 
+LOG_CHANNEL = "-1001896877147" 
 # MongoDB setup
-MONGO_URL = ""
-DB_NAME = ""
+MONGO_URL = "mongodb+srv://mikota4432:jkJDQuZH6o8pxxZe@cluster0.2vngilq.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "mongodb"
 
 DOWNLOAD_DIR = "./downloads"
 #ADMIN INFO
